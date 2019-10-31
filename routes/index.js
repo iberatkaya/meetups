5b791af19f7b85e8bb43a4bc0dcff902d8139d78
iberatkaya <beratkaya1998@gmail.com>
2019-11-01 01:34:01 +0300

    Added api requests

--- routes/index.js
+++ routes/index.js
@@ -6,33 +6,47 @@ var randomstring = require('randomstring');
 const DBSOURCE = "db.sqlite"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
-        console.log('Connected to the SQLite database.')
-        db.run('SELECT * FROM PEOPLE', [], (res, err) => {console.log(res);});
-    }
+  if (err) {
+    // Cannot open database
+    console.error(err.message)
+    throw err
+  } else {
+    console.log('Connected to the SQLite database.')
+    db.run('SELECT * FROM PEOPLE', [], (res, err) => { console.log(res); });
   }
+}
 );
 
 
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
   var randomstr = randomstring.generate(24);
   var name = req.body.name;
   var dates = req.body.dates;
   console.log(req.body);
-  db.serialize(function() {
-    db.run('INSERT INTO PEOPLE(key, name, dates) VALUES(?, ?, ?)', [randomstr, name, JSON.stringify(dates)], (res, err) => {console.log(res);});
+  db.serialize(function () {
+    db.run('INSERT INTO PEOPLE(key, name, dates) VALUES(?, ?, ?)', [randomstr, name, JSON.stringify(dates)], (res, err) => { console.log(res); });
   });
-  res.json({key: randomstr});
+  res.json({ key: randomstr });
 });
 
 /* GET home page. */
-router.get('/:key', function(req, res, next) {
-  console.log(req.body);
-  res.json({ title: 'Express', body: req.body, key: req.params.key });
+router.get('/:key', function (req, res, next) {
+  console.log(req.params.key);
+  db.serialize(function () {
+    db.all('SELECT * FROM PEOPLE WHERE key = ?', [req.params.key], (err, rows) => {
+      //      console.log(typeof(res));
+      res.json(rows);
+    });
+  });
+});
+
+router.post('/:key', function (req, res, next) {
+  console.log(req.params.key);
+  db.serialize(function () {
+    db.run('INSERT INTO PEOPLE(key, name, dates) VALUES(?, ?, ?)', [req.params.key, req.body.name, JSON.stringify(req.body.dates)], (result, err) => { console.log(result); 
+      res.json({success: "1"});     
+    });
+  });
 });
 
 module.exports = router;
